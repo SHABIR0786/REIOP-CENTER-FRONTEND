297a095f44a49a988bd466313f4aa0d314b7ab21
developer7132 <nicolasjin7132@outlook.com>
2021-03-22 23:04:45 +0800

    moved to the next laptop

--- src/pages/import.vue
+++ src/pages/import.vue
@@ -4,6 +4,7 @@
     >
         <b-form-file
             v-model="uploadedFile"
+            @change="previewFile"
             placeholder="Choose or drop a xlsx or csv file"
             drop-placeholder="Drop file here"
             accept=".xls, .xlsx, .csv"
@@ -15,6 +16,7 @@
 
 <script>
 import { mapGetters } from "vuex"
+import XLSX from "xlsx"
 
 export default {
     name: "Import",
@@ -29,10 +31,20 @@ export default {
             isCollapsed: 'uxModule/isCollapsed'
         })
     },
-    watch: {
-        uploadedFile () {
-            let data = new Uint8Array(this.uploadedFile)
-            console.log(data)
+    methods: {
+        previewFile (e) {
+            let files = e.target.files, f = files[0]
+            let reader = new FileReader()
+            reader.onload = (e) => {
+                var data = new Uint8Array(e.target.result);
+                var workbook = XLSX.read(data, {type: 'array'});
+                let sheetName = workbook.SheetNames[0]
+                /* DO SOMETHING WITH workbook HERE */
+                console.log(workbook);
+                let worksheet = workbook.Sheets[sheetName];
+                console.log(XLSX.utils.sheet_to_json(worksheet));
+            };
+            reader.readAsArrayBuffer(f);
         }
     }
 }
