b865baae6178a8fd8d8265db0401ac8b37bb0f91
Armen96 <barsegyan96armen@gmail.com>
2021-10-29 18:37:47 +0400

    Remove BG process

--- src/components/shared/NavBar.vue
+++ src/components/shared/NavBar.vue
@@ -11,9 +11,9 @@
                 <b-dropdown-item>
                     <router-link class="link-label" to="/labels"><b-icon icon="tools"></b-icon> Labels</router-link>
                 </b-dropdown-item>
-                <b-dropdown-item>
-                    <router-link class="link-label" to="/background-processes"><b-icon icon="gear-fill"></b-icon> Background Processes</router-link>
-                </b-dropdown-item>
+<!--                <b-dropdown-item>-->
+<!--                    <router-link class="link-label" to="/background-processes"><b-icon icon="gear-fill"></b-icon> Background Processes</router-link>-->
+<!--                </b-dropdown-item>-->
               <b-dropdown-item>
                 <router-link class="link-label" to="/errors"><b-icon icon="exclamation-circle-fill" variant="warning"></b-icon> Errors</router-link>
               </b-dropdown-item>
