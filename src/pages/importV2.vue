82a92f590a7e8b94f1caf450b178fbc8952568d2
vyasishanatc194 <vyasishanatc194@gmail.com>
2022-08-26 12:40:09 +0530

    pull data date changes

--- src/pages/importV2.vue
+++ src/pages/importV2.vue
@@ -236,10 +236,14 @@ export default {
       async handlePageClick(){
         this.$store.dispatch('uxModule/setLoading')
         
-        this.$refs.table.refresh();
+        if(this.$refs.table){
+            this.$refs.table.refresh();
+        }
         await this.showImports()
         // await this.$store.dispatch("importV2Module/getAllProcesses", {page: pageNumber, perPage: this.perPage});
-        this.$refs.table.refresh();
+        if(this.$refs.table){
+            this.$refs.table.refresh();
+          }
         this.$store.dispatch('uxModule/hideLoader');
       },
          
@@ -261,7 +265,9 @@ export default {
             let progresspercentage = Math.round((is_processed / (is_processed + is_processing)) * 100);
             let index = this.filteredItems.findIndex(x=>x.id == item.id);
             this.filteredItems[index].percentage = progresspercentage;
-            this.$refs.table.refresh();
+            if(this.$refs.table){
+              this.$refs.table.refresh();
+            }
             if(progresspercentage == 100) {
              clearInterval(this.intervalId);
             }
