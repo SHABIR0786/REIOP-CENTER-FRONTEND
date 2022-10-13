aaf08e201f822d31c4f217e06965a1fff3eb804d
HammadKhan7860 <hammadkhan.developer@gmail.com>
2022-10-14 01:13:44 +0500

    worked on seller module attempted_skip_trace_sources

--- src/components/export/EditExportModal.vue
+++ src/components/export/EditExportModal.vue
@@ -21,14 +21,14 @@
                         </b-row>
                         <b-row class="mb-1 text-center">
                             <b-col cols="12">
-                                <b-input-group prepend="Team Id" class="mb-2">
+                                <b-input-group prepend="Team Name" class="mb-2">
                                     <b-form-input readonly v-model="exportItem.team_name"></b-form-input>
                                 </b-input-group>
                             </b-col>
                         </b-row>
                         <b-row class="mb-1 text-center">
                             <b-col cols="12">
-                                <b-input-group prepend="User Id" class="mb-2">
+                                <b-input-group prepend="User Name" class="mb-2">
                                     <b-form-input readonly v-model="exportItem.user"></b-form-input>
                                 </b-input-group>
                             </b-col>
